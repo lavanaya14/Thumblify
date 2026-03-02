4c3629f082f498d7489b1784379517381caa11b7
Lavanaya Rajput <149952338+lavanaya14@users.noreply.github.com>
2026-03-03 01:35:42 +0530

    Delete server/dist/routes/UserRoutes.js

--- server/dist/routes/UserRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { getThumbnailbyId, getUsersThumbnails } from '../controllers/UserController.js';
-import protect from '../middlewares/auth.js';
-const UserRouter = express.Router();
-UserRouter.get('/thumbnails', protect, getUsersThumbnails);
-UserRouter.get('/thumbnail/:id', protect, getThumbnailbyId);
-export default UserRouter;
