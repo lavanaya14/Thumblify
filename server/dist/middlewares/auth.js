a446d97e53701b18f5b54c78e745ce1a47c3cff8
Lavanaya Rajput <149952338+lavanaya14@users.noreply.github.com>
2026-03-12 23:24:06 +0530

    Delete server/dist/middlewares directory

--- server/dist/middlewares/auth.js
+++
@@ -1,8 +0,0 @@
-const protect = async (req, res, next) => {
-    const { isLoggedIn, userId } = req.session;
-    if (!isLoggedIn || !userId) {
-        return res.status(401).json({ message: 'You are not logged in' });
-    }
-    next();
-};
-export default protect;
