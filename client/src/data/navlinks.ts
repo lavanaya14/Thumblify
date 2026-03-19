7eaf30a8b1bc820320481463e85ea16339a4ec95
Lavanaya Rajput <149952338+lavanaya14@users.noreply.github.com>
2026-03-19 23:49:12 +0530

    Delete client/src/data/navlinks.ts

--- client/src/data/navlinks.ts
+++
@@ -1,8 +0,0 @@
-import type { INavLink } from "../types";
-
-export const navlinks: INavLink[] = [
-    { name: "Home", href: "/" },
-    { name: "Features", href: "#features" },
-    { name: "Testimonials", href: "#testimonials" },
-    { name: "Pricing", href: "#pricing" },
-];
